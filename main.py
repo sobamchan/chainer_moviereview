299a4e4e2171b5cecfbe768be4cd850b425e7b02
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2017-06-02 14:33:59 +0900

    added cnn

--- main.py
+++ main.py
@@ -9,33 +9,14 @@ import numpy as np
 from tqdm import tqdm
 
 import argparse
-import os
+import os, sys
+sys.path.remove('/Users/sochan/project/sobamchan')
 
-from sobamchan.sobamchan_chainer import Model
-from sobamchan.sobamchan_moviereview import train
-from sobamchan.sobamchan_chainer_link import PreTrainedEmbedId
-
-class MLP(Model):
-    def __init__(self, class_n, vocab_n, d, vocab, fpath):
-        super(MLP, self).__init__(
-            embed=PreTrainedEmbedId(vocab_n, d, vocab, fpath, False),
-            fc1=L.Linear(None, 10),
-            fc2=L.Linear(10, 10),
-            fc3=L.Linear(10, class_n),
-        )
-    def __call__(self, x, t, train=True):
-        x = self.fwd(x, train)
-        return F.softmax_cross_entropy(x, t), F.accuracy(x, t)
-    def fwd(self, x, train):
-        h = self.embed(x)
-        h = F.relu(self.fc1(h))
-        h = F.relu(self.fc2(h))
-        h = self.fc3(h)
-        return h
+from model import MLP, CNN
 
 
 if __name__ == '__main__':
     opts = {}
-    opts['model'] = MLP
+    opts['model'] = CNN
     opts['optimizer'] = optimizers.AdaGrad()
     train(opts)
---
+++ model.py
@@ -0,0 +1,46 @@
+from sobamchan.sobamchan_chainer import Model
+from sobamchan.sobamchan_moviereview import train
+from sobamchan.sobamchan_chainer_link import PreTrainedEmbedId
+
+class MLP(Model):
+    def __init__(self, class_n, vocab_n, d, vocab, fpath):
+        super(MLP, self).__init__(
+            embed=PreTrainedEmbedId(vocab_n, d, vocab, fpath, False),
+            fc1=L.Linear(None, 10),
+            fc2=L.Linear(None, 10),
+            fc3=L.Linear(None, class_n),
+        )
+    def __call__(self, x, t, train=True):
+        x = self.fwd(x, train)
+        return F.softmax_cross_entropy(x, t), F.accuracy(x, t)
+    def fwd(self, x, train):
+        h = self.embed(x)
+        h = F.tanh(self.fc1(h))
+        h = F.tanh(self.fc2(h))
+        h = self.fc3(h)
+        return h
+
+
+class CNN(Model):
+
+    def __init__(self, class_n, vocab_n, d, vocab, fpath):
+        super(CNN, self).__init__(
+            embed=PreTrainedEmbedId(vocab_n, d, vocab, fpath, False),
+            conv1=L.Convolution2D(1, 16, (3, 1)),
+            conv2=L.Convolution2D(16, 3, (3, 1)),
+            fc=L.Linear(None, class_n)
+        )
+
+    def __call__(self, x, t, train=True):
+        x = self.fwd(x, train)
+        return F.softmax_cross_entropy(x, t), F.accuracy(x, t)
+
+    def fwd(self, x, train):
+        h = self.embed(x)
+        batch, height, width = h.shape
+        h = F.reshape(h, (batch, 1, height, width))
+        h = self.conv1(h)
+        h = self.conv2(h)
+        h = F.max_pooling_2d(h, (1, 3))
+        h = self.fc(h)
+        return h
